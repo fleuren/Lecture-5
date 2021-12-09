66784c88307504a7ee650b0ddcfcd59c81a8e307
fleuren <91404708+fleuren@users.noreply.github.com>
2021-12-09 18:32:37 +0400

    Add files via upload

---
+++ lec5.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+	//Letter Checker
+	// USE ISALPHA, U BRICK WALL >:c
+/*
+cout << "Input a letter from A-Z.";
+string letter;
+cin >> letter;
+
+
+if (letter == "a"){
+	cout << "The letter is a vowel";
+}
+
+else if (letter == "e"){
+	cout << "The letter is a vowel";
+}
+
+else if (letter == "i"){
+	cout << "The letter is a vowel";
+}
+
+else if (letter == "o"){
+	cout << "The letter is a vowel";
+}
+
+else if (letter == "u"){
+	cout << "The letter is a vowel";
+}
+
+else {
+	cout << "The letter is a consonant";
+}
+*/
+
+/*
+bool musicalfriend = true;
+
+int instrumentinput;
+
+cout << "Please choose between 'drums' or 'guitar'." << endl;
+cout << "1 - drums" << endl;
+cout << "2 - guitar" << endl;
+cin >> instrumentinput;
+
+if (musicalfriend = true) {
+	if ((instrumentinput == 1) || (instrumentinput == 2)){
+		cout << "Friend can join the band";
+	}else{
+		cout << "Friend must play either drums or guitar";
+	}	
+}
+	else{
+	cout << "Non-musical friend cannot join the band";
+}
+*/
+return 0;
+}
